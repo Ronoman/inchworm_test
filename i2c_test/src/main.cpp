96a90d4311e4ee9b437115e123435bab7a3d7037
Eli Benevedes <benevedeselias@gmail.com>
2022-03-22 18:12:34 -0400

    I2C and NFC tests

---
+++ i2c_test/src/main.cpp
@@ -0,0 +1,57 @@
+#include <Arduino.h>
+#include <Wire.h> //include Wire.h library
+#include <HardwareSerial.h>
+
+void setup()
+{
+  Wire.begin(); // Wire communication begin
+  Serial.begin(9600); // The baudrate of Serial monitor is set in 9600
+  while (!Serial); // Waiting for Serial Monitor
+  Serial.println("\nI2C Scanner");
+
+  pinMode(29, OUTPUT);
+  digitalWrite(29, HIGH);
+}
+
+void loop()
+{
+  uint8_t error, address; //variable for error and I2C address
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for (address = 1; address < 127; address++ )
+  {
+    //Serial.println("Scanning");
+    //Serial.println(address);
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    //Serial.println("Begun");
+    error = Wire.endTransmission();
+    //Serial.println("End");
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.print(address, HEX);
+      Serial.println("  !");
+      nDevices++;
+    }
+    else if (error == 4)
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.println(address, HEX);
+    }
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+}
